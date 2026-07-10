00461d678f3951c0a13af465c83ed00ecd245533
Rolle Laukkarinen <roni@dude.fi>
2026-07-10 23:53:20 +0300

    Restyle front-page hero as a terminal prompt with JetBrains Mono

--- content/themes/rolledesign/front-page.php
+++ content/themes/rolledesign/front-page.php
@@ -18,8 +18,8 @@ get_header(); ?>
 <main class="site-main transition-fade">
   <section class="block block-hero block-hero-front block-has-space" id="hero">
     <div class="content">
-      <h1 id="content"><span>Ron</span> <span>the</span> <span>don.</span></h1>
-      <p>Just say <span>Rolle</span>. My full name is Rolle Laukkarinen and I'm a web designer and developer from Finland.</p>
+      <h1 id="content"><span>$</span> <span>sudo su</span> <span>rolle.</span></h1>
+      <p>Hello! I'm Rolle, nice to meet you! I'm a programmer, developer, designer, sysadmin from Finland.</p>
 
       <img src="https://www.gravatar.com/avatar/<?php echo md5( strtolower( trim( 'rolle@dude.fi' ) ) ); ?>.jpg?s=300" alt="Facial image of me, Rolle Laukkarinen aka rolle from Twitter" class="gravatar" width="100" height="100" />
       <p class="gravatar-description">Yep, that's my current gravatar. More recent photos at <a class="instagram-link" href="https://www.instagram.com/rolle_/" aria-label="Instagram"><?php include get_theme_file_path( '/svg/instagram.svg' ); ?> Instagram</a>.
